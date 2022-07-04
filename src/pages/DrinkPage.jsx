a76b24e4a333cc5297a13f01537ee8b345157b79
pbsuazocod <pbsuazocod@gmail.com>
2022-07-04 15:32:46 -0800

    animation of ingredients

--- src/pages/DrinkPage.jsx
+++ src/pages/DrinkPage.jsx
@@ -9,10 +9,18 @@ function DrinkPage() {
     const item = data.find(item => item.id === id);
 
     return (
-        <a href="/" className=''>
-            <div className={`selection bg-drink-${id} bg-no-repeat bg-center bg-cover`}>
-            </div>
-        </a>
+        <>
+            <a href="/" className=''>
+                <div className={`selection bg-drink-${id} flex flex-col justify-end bg-no-repeat bg-center bg-cover`}>
+                    <div className='ingredients-box'>
+                        <div className="flex items-center flex-col">
+                            <img src={require(`../img/ingredients/${id}.png`)} className='ingredients' />
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </>
+
     )
 
 }
