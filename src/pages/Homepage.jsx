ae39520dad4390de0822b0da2382ebc34f757b2b
pbsuazocod <pbsuazocod@gmail.com>
2022-07-06 07:30:36 -0800

    fix audio and bottum img

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,32 +3,37 @@ import Footer from "../components/Footer"
 import useSound from 'use-sound'
 import mySound from '../sound/Fashion Show.wav'
 import { GiSpeaker } from 'react-icons/gi';
+import { useState } from 'react';
 
 
 function Homepage() {
 
 
     const [play, { stop }] = useSound(mySound);
+    const [soundState, setsoundState] = useState("stop")
+
 
 
     return (
-        <div className={`bg-homescreen relative selection bg-no-repeat bg-center bg-cover flex flex-col`}>
+        <div className={`bg-homescreen relative selection bg-no-repeat bg-center bg-cover flex flex-col items-center justify-between`}>
 
-            {/* <a className=' music' onClick={() => play()} onCLick={() => stop()}>
+            <a className='music' onClick={() => {
+                if (soundState === "stop") {
+                    play()
+                    setsoundState("play")
+                } else {
+                    stop()
+                    setsoundState("stop")
+                }
+            }}>
                 <span className='bg-green-300' role="img" aria-label="trumpet" >
-                    <GiSpeaker className='speaker w-screen' />
+                    <GiSpeaker className='speaker ' />
                 </span>
-            </a> */}
-
-            <div className="flex-1"></div>
-
+            </a>
 
             <a alt='select your drink' href="/selection" className='selection-bottom'>
                 <img alt='Selected cocktail' src={require(`../img/buttom.png`)} className='bottom-selection-img' />
             </a>
-
-
-
 
             <Footer />
         </div>
