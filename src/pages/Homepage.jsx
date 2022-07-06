cb9ef7e0de55e382db8c52afde32dd8894933d41
pbsuazocod <pbsuazocod@gmail.com>
2022-07-06 05:36:57 -0800

    added sound

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import Footer from "../components/Footer"
+import useSound from 'use-sound'
+import mySound from '../sound/Fashion Show.wav'
+import { GiSpeaker } from 'react-icons/gi';
 
 
 function Homepage() {
+
+
+    const [play, { stop }] = useSound(mySound);
+
+
     return (
-        <div className={`bg-homescreen selection bg-no-repeat bg-center bg-cover flex flex-col`}>
-            <a alt='select your drink' href="/selection" className='flex-1'></a>
+        <div className={`bg-homescreen relative selection bg-no-repeat bg-center bg-cover flex flex-col`}>
+
+            <a className=' music' onClick={() => play()} onCLick={() => stop()}>
+                <span className='bg-green-300' role="img" aria-label="trumpet" >
+                    <GiSpeaker className='speaker w-screen' />
+                </span>
+            </a>
+
+            <div className="flex-1"></div>
+
+
+            <a alt='select your drink' href="/selection" className='selection-bottom'>
+                <img alt='Selected cocktail' src={require(`../img/buttom.png`)} className='bottom-selection-img' />
+            </a>
+
+
+
+
             <Footer />
         </div>
     )
--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import useSound from 'use-sound'
 import mySound from '../sound/Fashion Show.wav'
+import { FaMusic } from 'react-icons/fa';
 
 
 function Test() {
     const [play, { stop }] = useSound(mySound);
 
     return (
-        <a onMouseEnter={() => play()} onMouseLeave={() => stop()}>
-            <span role="img" aria-label="trumpet">
-                🎺
+        <a className='' onClick={() => play()} onCLick={() => stop()}>
+            <span className='bg-green-300' role="img" aria-label="trumpet" >
+                <FaMusic className='h-5 w-screen text-blue-800' />
             </span>
         </a>
 
